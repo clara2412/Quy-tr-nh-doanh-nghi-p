339a4490d056b1a2a4c0705476919840f187c12c
HLOEY <70062580+HLoey258@users.noreply.github.com>
2022-06-02 20:37:22 +0700

    add CSKH

---
+++ Các quy trình/Quy trình CSKH/workers/sendEmail-urban.js
@@ -0,0 +1,53 @@
+const { Client, logger } = require("camunda-external-task-client-js");
+const { Variables } = require("camunda-external-task-client-js");
+
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+
+const client = new Client(config);
+
+client.subscribe("sendEmail-urban", async function({ task, taskService }) {
+  let bookName = task.variables.get("bookName");
+  let address = task.variables.get("customerAddress");
+  let dayReceive = task.variables.get("dayReceive");
+  let customerEmail = task.variables.get("customerEmail");
+  let cityType = task.variables.get("cityType");
+
+
+
+  var nodemailer = require('nodemailer');
+  var transporter = nodemailer.createTransport({   
+    service: 'gmail',
+    auth: {
+      user: 'longhai.hua1006@gmail.com',
+      pass: 'Game0988'
+    },
+    tls:{
+          rejectUnauthorized: false
+    }
+  });
+  
+  var mailOptions = {
+    from: 'longhai.hua1006@gmail.com',
+    to: `${customerEmail}`,
+    subject: 'CONFIRM ORDER - BUYING BOOK',
+    text: `
+    YOUR ORDER HAS BEEN RECEIVED!!
+    This is your order:
+    Name of the Book: ${bookName}
+    Address: ${address}
+    Day Receive: ${dayReceive}
+    Type of City: ${cityType}
+    Please reply 'Yes that is my order' to complete the confimation!
+    `
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+
+  await taskService.complete(task);
+});
